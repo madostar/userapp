db1d344625fd367f54b992eae7eb4b88c4f94392
madostar <medu2013@gmail.com>
2021-12-10 15:50:14 +0200

    dotnet files
    Initial commit of dotnet files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import '@progress/kendo-theme-default/dist/all.css';
+import { Calendar } from '@progress/kendo-react-dateinputs'
+import { process } from '@progress/kendo-data-query';
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import products from './products.json';
+import userlist from './userlist.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello KendoReact!</h1>
+        {/* <Calendar/> */}
+        {/* <Grid
+          data={products}>
+          <GridColumn field="ProductName" />
+          <GridColumn field="UnitPrice" />
+          <GridColumn field="UnitsInStock" />
+          <GridColumn field="Discontinued" />
+        </Grid> */}
+        <Grid data={userlist}>
+          <GridColumn field='Username'/>
+          <GridColumn field='FullName'/>
+          <GridColumn field='LastLogin'/>
+          <GridColumn field='Enabled'/>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
